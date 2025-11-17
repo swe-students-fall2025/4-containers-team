276a524e21c02607a665852489ac3aff3f236777
evemak <136145873+evemak@users.noreply.github.com>
2025-11-16 22:42:49 -0500

    renamed language learner file, updated main

---
+++ machine-learning-client/language_learner.py
@@ -0,0 +1,26 @@
+# The actual machine learning logic
+import whisper
+from database import save_sample
+
+model = whisper.load_model("tiny")
+
+
+def detect_language_from_audio(filepath):
+    result = model.transcribe(filepath)
+    transcript = result.get("text", "").strip()
+    lang = result.get(
+        "language", "unknown"
+    )  # unkown is a place holder, whisper handles the language detection
+
+    # saves to MongoDB
+    save_sample(
+        audio_path=filepath,
+        transcript=transcript,
+        lang=lang,
+    )
+
+    # this is to display for web app front end
+    return {
+        "language": lang,
+        "transcript": transcript,
+    }
--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,4 +1,11 @@
 # Machine Learning orchestrator
+import argparse
+import os
+import sys
+import json
+
+from language_learner import detect_language_from_audio
+
 
 if __name__ == "__main__":
     print("Machine Learning Client started")
