3da6927480ed87509e6b53c3678ab631fec78d73
evemak <136145873+evemak@users.noreply.github.com>
2025-11-16 23:51:42 -0500

    debug

--- machine-learning-client/database.py
+++ machine-learning-client/database.py
@@ -1,4 +1,9 @@
-# database 
-def save_sample(audio_path: str, transcript: str, lang: str | None = None) -> None:
-    return
+# database
 
+def save_result(
+    audio_path: str, language: str, transcript: str | None = None
+):
+    """
+    to be implemented (save result is used in main.py in order to save language detection results into database)
+    """
+    pass
