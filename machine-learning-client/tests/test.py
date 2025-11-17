50c7d70b63bf1ca06d4b7dbfaa1da956c4b4b4f1
evemak <136145873+evemak@users.noreply.github.com>
2025-11-17 02:25:07 -0500

    created __init__.py and debugged

--- machine-learning-client/tests/test.py
+++ machine-learning-client/tests/test.py
@@ -1,16 +1,8 @@
 """Unit tests for machine learning client."""
 
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parent.parent
-if str(ROOT) not in sys.path:
-    sys.path.insert(0, str(ROOT))
-
-
 import sys
 import os
-import language_learner
-import database
+import main
 
 
 class TestMain:
