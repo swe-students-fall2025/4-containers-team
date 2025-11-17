16b9762fff9ce7e5792d65fb56bfdce31b9f4b7a
MajesticSeagull26 <wt2187@nyu.edu>
2025-11-16 22:33:45 -0500

    Fix linting

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,8 +1,10 @@
-# Machine Learning orchestrator
+"""Machine Learning orchestrator."""
+
 
 def main():
+    """Main entry point for ML client"""
     return
+
 
 if __name__ == "__main__":
     main()
-    
